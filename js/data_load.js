23c841b78c4a2c9496827b29d45397ff536811f2
jjecgd <jecgdd@naver.com>
2019-05-05 02:18:16 +0900

    first

---
+++ js/data_load.js
@@ -0,0 +1,107 @@
+var subject_name = '취업촉진';
+var subject_code = 'gino02';
+var chasi_num = '02';
+var maxPage = 1; //총 페이지 갯수
+var move_value = [1]; /* 인덱스 넘어갈 페이지 번호 */
+var down_file = '강의노트_' + chasi_num + '회차.zip'; // 다운로드자료 파일 이름
+var headerStr = '';
+var menuStr = '';
+var controlStr = '';
+var learningMapStr = '';
+var page_title_list = [];
+
+page_title_list[0] = ['', ''];
+
+headerStr += '<div class="title">빅데이터 영상</div>';
+headerStr += '<p class="subject">빅데이터</p>';
+
+menuStr += '<div id="menu_open"></div>';
+menuStr += '<div class="box box1">';
+menuStr += '    <div class="title"><p>학습하기</p></div>';
+menuStr += '   <ul class="items">';
+menuStr += '        <li id="index-move1"><p>플레이 영상</p></li>';
+menuStr += '   </ul>';
+menuStr += '</div>';
+
+learningMapStr += '<div id="learning_map_wrap">';
+learningMapStr += '    <div id="learning_map">';
+learningMapStr += '        <div id="learning_map_close"></div>';
+learningMapStr += '        <dl id="list1">';
+learningMapStr += '        	   <dt><p>경영철학의 개념</p></dt>';
+learningMapStr += '        	   <dd id="chasi01"><p>01. 훈련생 선발</p></dd>';
+learningMapStr += '        	   <dd id="chasi02"><p>02. 취업지원체계</p></dd>';
+learningMapStr += '        	   <dd id="chasi03"><p>03. 취업상담</p></dd>';
+learningMapStr += '        	   <dd id="chasi04"><p>04. 취업정보</p></dd>';
+learningMapStr += '        </dl>';
+learningMapStr += '    </div>';
+learningMapStr += '</div>';
+
+controlStr += '<div id="quickBubble"></div>';
+controlStr += '<div class="control_wrap">';
+//controlStr += '    <div id="area_watermark"></div>';
+controlStr += '    <div id="area_sub">';
+controlStr += '        <div class="btn_wrap learningmap">';
+controlStr += '            <button id="btn_learningmap"></button>';
+controlStr += '            <div class="btn_bubble">러닝맵</div>';
+controlStr += '        </div>';
+controlStr += '        <div class="btn_wrap download">';
+controlStr += '            <button id="btn_download"></button>';
+controlStr += '            <div class="btn_bubble">학습노트</div>';
+controlStr += '        </div>';
+controlStr += '    </div>';
+controlStr += '    <div id="area_bar">';
+controlStr += '        <div id="bar_display_bg"><div id="bar_display_mask"><div id="bar_display_fill"></div></div></div>';
+controlStr += '        <div id="bar_event"></div>';
+controlStr += '        <div id="quickMark"></div>';
+controlStr += '        <div id="bar_pointer"></div>';
+controlStr += '    </div>';
+controlStr += '    <div id="area_time">';
+controlStr += '        <div id="cTime">00:00</div>';
+controlStr += '        <div id="slice">/</div>';
+controlStr += '        <div id="tTime">00:00</div>';
+controlStr += '    </div>';
+controlStr += '    <div id="area_controls">';
+controlStr += '        <div class="btn_wrap play">';
+controlStr += '            <button id="btn_play"></button>';
+controlStr += '            <div class="btn_bubble">재생</div>';
+controlStr += '        </div>';
+controlStr += '        <div class="btn_wrap pause">';
+controlStr += '            <button id="btn_pause"></button>';
+controlStr += '            <div class="btn_bubble">일시정지</div>';
+controlStr += '        </div>';
+controlStr += '        <div class="btn_wrap replay">';
+controlStr += '            <button id="btn_replay"></button>';
+controlStr += '            <div class="btn_bubble">다시보기</div>';
+controlStr += '        </div>';
+controlStr += '        <div class="btn_wrap script">';
+controlStr += '            <button id="btn_script"></button>';
+controlStr += '            <div class="btn_bubble">자막보기</div>';
+controlStr += '        </div>';
+controlStr += '        <div class="btn_wrap sound">';
+controlStr += '            <button id="btn_sound"><span id="sound_mute"></span></button>';
+controlStr += '            <div class="btn_bubble">소리 끔</div>';
+controlStr += '            <div id="sound_wave">';
+controlStr += '                <div id="sound_display_bg"><div id="sound_display_mask"><div id="sound_display_fill"></div></div></div>';
+controlStr += '                <div id="sound_event"></div>';
+controlStr += '                <div id="sound_pointer"></div>';
+controlStr += '            </div>';
+controlStr += '        </div>';
+controlStr += '        <div id="area_speed">';
+controlStr += '            <div id="display_speed">×1.0</div>';
+controlStr += '            <ul id="speed_list">';
+controlStr += '                <li class="btn_speed" data-speed="0.8">×0.8</li>';
+controlStr += '                <li class="btn_speed" data-speed="1.0">×1.0</li>';
+controlStr += '                <li class="btn_speed" data-speed="1.5">×1.5</li>';
+controlStr += '                <li class="btn_speed" data-speed="2.0">×2.0</li>';
+controlStr += '            </ul>';
+controlStr += '        </div>';
+controlStr += '    </div>';
+controlStr += '    <div id="area_movePage">';
+controlStr += '        <div id="btn_prev"></div>';
+controlStr += '        <div id="currentPage"></div>';
+controlStr += '        <div id="slice">/</div>';
+controlStr += '        <div id="totalPage"></div>';
+controlStr += '        <div id="btn_next"></div>';
+controlStr += '    </div>';
+controlStr += '    <div id="next_bubble"></div>'
+controlStr += '</div>';
---
+++ js/script.js
@@ -0,0 +1,2 @@
+﻿var scriptValue = [];
+scriptValue[0] = "본 페이지에서는 스크립트가 제공되지 않습니다.";
